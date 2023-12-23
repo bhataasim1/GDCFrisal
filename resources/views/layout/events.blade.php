34db2eb062237671d09bb082deebd830c358b17f
Bhat Aasim <bhataasim700@gmail.com>
2023-12-23 19:59:29 +0530

    added Events Page

---
+++ resources/views/layout/events.blade.php
@@ -0,0 +1,55 @@
+<h1 class="flex justify-center items-center py-3 rounded-xl text-white font-bold text-lg mt-2 mb-2"
+    style="background-color: #f48000">Events</h1>
+
+<div class="container mx-auto p-4 rounded-xl border-t-4 border-b-4 border-yellow-700 mt-2 relative overflow-hidden">
+    <div class="max-w-full overflow-x-auto">
+        <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+
+            <!-- Sample Box 1 with Clickable Image and Button -->
+            <a href="#"
+                class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
+                <img src="{{ asset('images/Societies/greenSociety.png') }}" alt="Event Logo" class="w-full h-full object-cover">
+                <span
+                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity opacity-0 hover:opacity-100">
+                    Green Society
+                </span>
+            </a>
+            <!-- End of Sample Box 1 -->
+
+            <!-- Sample Box 2 (You can duplicate this for more boxes) -->
+            <a href="another-link-url"
+                class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
+                <img src="{{ asset('images/Societies/Heritage.jpg') }}" alt="Image Description"
+                    class="w-full h-full object-cover">
+                <span
+                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity opacity-0 hover:opacity-100">
+                    Cultural Society
+                </span>
+            </a>
+            <!-- End of Sample Box 1 -->
+
+            <!-- Sample Box 1 with Clickable Image and Button -->
+            <a href="your-link-url"
+                class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
+                <img src="{{ asset('images/a2.jpeg') }}" alt="Event Logo" class="w-full h-full object-cover">
+                <span
+                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity opacity-0 hover:opacity-100">
+                    Heritage Society
+                </span>
+            </a>
+            <!-- End of Sample Box 1 -->
+
+            <!-- Sample Box 1 with Clickable Image and Button -->
+            <a href="your-link-url"
+                class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
+                <img src="{{ asset('images/a2.jpeg') }}" alt="Event Logo" class="w-full h-full object-cover">
+                <span
+                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity opacity-0 hover:opacity-100">
+                    Linguistic Society
+                </span>
+            </a>
+            <!-- End of Sample Box 1 -->
+
+        </div>
+    </div>
+</div>
