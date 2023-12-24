9dfbb78eee79bf8401da77b2625618e8aaf56b13
Bhat Aasim <bhataasim700@gmail.com>
2023-12-24 19:06:22 +0530

    events page Updated

--- resources/views/layout/events.blade.php
+++ resources/views/layout/events.blade.php
@@ -5,41 +5,55 @@
     <div class="max-w-full overflow-x-auto">
         <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
 
-            <!-- Sample Box 1 with Clickable Image and Button -->
+            <!-- Sample Box -->
             <a href="#"
                 class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
-                <img src="{{ asset('images/Societies/greenSociety.png') }}" alt="Event Logo" class="w-full h-full object-cover">
+                <img src="{{ asset('images/Societies/greenSociety.png') }}" alt="Event Logo"
+                    class="w-full h-full object-cover">
                 <span
-                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity opacity-0 hover:opacity-100">
+                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity">
                     Green Society
                 </span>
             </a>
-            <!-- End of Sample Box 1 -->
+            <!-- End -->
 
-            <!-- Sample Box 2 (You can duplicate this for more boxes) -->
+            <!-- Sample Box -->
             <a href="#"
                 class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
-                <img src="{{ asset('images/Societies/Heritage.jpg') }}" alt="Image Description"
+                <img src="{{ asset('images/Societies/Heritage.jpg') }}" alt="Event Logo"
                     class="w-full h-full object-cover">
                 <span
-                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity opacity-0 hover:opacity-100">
-                    Cultural Society
-                </span>
-            </a>
-            <!-- End of Sample Box 1 -->
-
-            <!-- Sample Box 1 with Clickable Image and Button -->
-            <a href="#"
-                class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
-                <img src="{{ asset('images/a2.jpeg') }}" alt="Event Logo" class="w-full h-full object-cover">
-                <span
-                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity opacity-0 hover:opacity-100">
+                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity">
                     Heritage Society
                 </span>
             </a>
-            <!-- End of Sample Box 1 -->
+            <!-- End -->
 
-            <!-- Sample Box 1 with Clickable Image and Button -->
+            <!-- Sample Box -->
+            <a href="#"
+                class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
+                <img src="{{ asset('images/Societies/greenSociety.png') }}" alt="Event Logo"
+                    class="w-full h-full object-cover">
+                <span
+                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity">
+                    Green Society
+                </span>
+            </a>
+            <!-- End -->
+
+            <!-- Sample Box -->
+            <a href="#"
+                class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
+                <img src="{{ asset('images/Societies/greenSociety.png') }}" alt="Event Logo"
+                    class="w-full h-full object-cover">
+                <span
+                    class="absolute left-0 w-full text-center p-2 bg-opacity-75 bg-gray-800 text-white font-bold transition-opacity">
+                    Green Society
+                </span>
+            </a>
+            <!-- End -->
+
+            {{-- <!-- Sample Box -->
             <a href="#"
                 class="relative overflow-hidden border-2 border-gray-300 rounded-md h-40 flex items-center justify-center transition-opacity hover:opacity-75">
                 <img src="{{ asset('images/a2.jpeg') }}" alt="Event Logo" class="w-full h-full object-cover">
@@ -48,7 +62,7 @@
                     Linguistic Society
                 </span>
             </a>
-            <!-- End of Sample Box 1 -->
+            <!-- End --> --}}
 
         </div>
     </div>
